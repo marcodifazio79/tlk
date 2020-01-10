76d9fa889a8da4f72507e3f64b1c053d19afcaf2
Francesco Di Fazio <fdifazio@fdifazio.dedempvp.local>
2020-01-10 14:30:46 +0100

    wip

--- serverMark2.py
+++ serverMark2.py
@@ -1,37 +1,64 @@
-import socket
-import sys
-import threading
+# import socket programming library 
+import socket 
+
+# import thread module 
+from _thread import *
+import threading 
+
+print_lock = threading.Lock() 
+
+# thread fuction 
+def threaded(c): 
+	while True: 
+
+		# data received from client 
+		#print ('test')
+		data = c.recv(8) 
+		if not data: 
+			print('No data, bye') 
+			
+			# lock released on exit 
+			print_lock.release() 
+			break
+		print ('received "%s"' % data)        
+         
+
+	# connection closed 
+	c.close() 
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def Main(): 
+	host = "10.10.10.71" 
+	
+	# reverse a port on your computer 
+	# in our case it is 10000 but it 
+	# can be anything 
+	port = 9000
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	s.bind((host, port)) 
+	print("socket binded to port", port) 
 
-# Bind the socket to the port
-server_address = ('10.10.10.71', 9000)
-print (sys.stderr, 'starting up on %s port %s' % server_address)
-sock.bind(server_address)
+	# put the socket into listening mode 
+	s.listen(5) 
+	print("socket is listening") 
 
-# Listen for incoming connections
-sock.listen(1)
+	# a forever loop until client wants to exit 
+	while True: 
 
-# Wait for a connection
-print (sys.stderr, 'waiting for a connection')
-connection, client_address = sock.accept()
+		# establish connection with client 
+		c, addr = s.accept() 
 
-def receive_and_print():
-        for message in iter(lambda: sock.recv(1024).decode(), ''):
-            print("From the modem: ", message)
-            
+		# lock acquired by client 
+		print_lock.acquire() 
+		print('Connected to :', addr[0], ':', addr[1]) 
+
+		# Start a new thread and return its identifier 
+		start_new_thread(threaded, (c,)) 
+        while True:
+		    data = input("cmd for server: ") 
+		    c.send(data.encode())
+	s.close() 
 
 
-try:
-    print (sys.stderr, 'connection from', client_address)
-    
-    background_thread = threading.Thread(target=receive_and_print)
-    background_thread.daemon = True
-    background_thread.start()
-
-    while 1:
-        data = input("insert command: ").encode() 
-        sock.send( data )
-finally:
-    x=0
+if __name__ == '__main__': 
+	Main() 
