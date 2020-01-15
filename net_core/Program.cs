bed9a5a182306fc99a904e144bd2297036e98af3
Francesco Di Fazio <fdifazio@live.com>
2020-01-15 09:25:05 +0100

    wip

--- net_core/Program.cs
+++ net_core/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace net_core
 {
     class Program
     {
+        private static Socket ConnectSocket()
+        {
+            Socket s = null;
+            int port = 9000;
+            IPAddress address = IPAddress.Parse("10.10.10.71");
+            
+            IPEndPoint ipe = new IPEndPoint(address, port);
+            Socket tempSocket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            tempSocket.Connect(ipe);
+
+            if(tempSocket.Connected)
+            {
+                s = tempSocket;
+               
+            }
+            else
+            {
+                
+            }
+            
+            return s;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Byte[] bytesReceived = new Byte[256];
+            string receivedString = "";
+            using(Socket s = ConnectSocket()) {
+                if (s == null){
+                    Console.WriteLine ("Connection failed");
+                    }
+                else{
+                    int bytes = 0;
+                    do {
+                        bytes = s.Receive(bytesReceived, bytesReceived.Length, 0);
+                        receivedString = Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+                        Console.WriteLine (receivedString);
+                    }
+                    while (bytes > 0);
+                }
+
+            }
         }
     }
 }
