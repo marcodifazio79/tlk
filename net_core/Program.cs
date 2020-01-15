da5cc6822a393c7a95cd2e973947cc35d807306c
Francesco Di Fazio <fdifazio@live.com>
2020-01-15 09:33:22 +0100

    wip

--- net_core/Program.cs
+++ net_core/Program.cs
@@ -8,49 +8,76 @@ namespace net_core
 {
     class Program
     {
-        private static Socket ConnectSocket()
-        {
-            Socket s = null;
-            int port = 9000;
-            IPAddress address = IPAddress.Parse("10.10.10.71");
-            
-            IPEndPoint ipe = new IPEndPoint(address, port);
-            Socket tempSocket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-            tempSocket.Connect(ipe);
-
-            if(tempSocket.Connected)
-            {
-                s = tempSocket;
-               
-            }
-            else
-            {
-                
-            }
-            
-            return s;
-        }
         static void Main(string[] args)
         {
             
-            Byte[] bytesReceived = new Byte[256];
-            string receivedString = "";
-            using(Socket s = ConnectSocket()) {
-                if (s == null){
-                    Console.WriteLine ("Connection failed");
-                    }
-                else{
-                    int bytes = 0;
-                    do {
-                        bytes = s.Receive(bytesReceived, bytesReceived.Length, 0);
-                        receivedString = Encoding.ASCII.GetString(bytesReceived, 0, bytes);
-                        Console.WriteLine (receivedString);
-                    }
-                    while (bytes > 0);
-                }
+            TcpListener server=null;   
+    try
+    {
+      // Set the TcpListener on port 9000.
+      Int32 port = 9000;
+      IPAddress localAddr = IPAddress.Parse("10.10.10.71");
+      
+      // TcpListener server = new TcpListener(port);
+      server = new TcpListener(localAddr, port);
 
-            }
+      // Start listening for client requests.
+      server.Start();
+         
+      // Buffer for reading data
+      Byte[] bytes = new Byte[256];
+      String data = null;
+
+      // Enter the listening loop.
+      while(true) 
+      {
+        Console.Write("Waiting for a connection... ");
+        
+        // Perform a blocking call to accept requests.
+        // You could also user server.AcceptSocket() here.
+        TcpClient client = server.AcceptTcpClient();            
+        Console.WriteLine("Connected!");
+
+        data = null;
+
+        // Get a stream object for reading and writing
+        NetworkStream stream = client.GetStream();
+
+        int i;
+
+        // Loop to receive all the data sent by the client.
+        while((i = stream.Read(bytes, 0, bytes.Length))!=0) 
+        {   
+          // Translate data bytes to a ASCII string.
+          data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+          Console.WriteLine("Received: {0}", data);
+       
+          // Process the data sent by the client.
+          data = data.ToUpper();
+
+          byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+          // Send back a response.
+          //stream.Write(msg, 0, msg.Length);
+          //Console.WriteLine("Sent: {0}", data);            
+        }
+         
+        // Shutdown and end connection
+        client.Close();
+      }
+    }
+    catch(SocketException e)
+    {
+      Console.WriteLine("SocketException: {0}", e);
+    }
+    finally
+    {
+       // Stop listening for new clients.
+       server.Stop();
+    }
+
+    Console.WriteLine("\nHit enter to continue...");
+    Console.Read();
         }
     }
 }
