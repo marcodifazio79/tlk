e2440116f6ed73dfecd7cd3d2a59c0912101cb46
FrancescoDF <fmcfazio@gmail.com>
2020-02-10 16:29:00 +0100

    wip

--- net_core_v2/Program.cs
+++ net_core_v2/Program.cs
@@ -105,7 +105,8 @@ public class AsynchronousSocketListener {
                 insertIntoDB(IPAddress.Parse (((IPEndPoint)handler.RemoteEndPoint).Address.ToString ()) + " send "+ content.Length.ToString() + " bytes, data : " + content);
         
 
-                Thread t = new Thread(()=>Send (handler, "#PSW123456"));
+                Thread t = new Thread(()=>Send (handler, "#PWD123456#G64,172.16.136.120:9000,iaiekl"));
+                
                 t.Start();
                 
             }
