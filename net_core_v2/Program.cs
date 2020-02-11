6f1f3de1b608af6b6912494510e44c0b93427ace
Francesco Di Fazio <fdifazio@live.com>
2020-02-11 10:10:02 +0100

    wip

--- net_core_v2/Program.cs
+++ net_core_v2/Program.cs
@@ -104,8 +104,12 @@ public class AsynchronousSocketListener {
                 Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",content.Length, content);
                 insertIntoDB(IPAddress.Parse (((IPEndPoint)handler.RemoteEndPoint).Address.ToString ()) + " send "+ content.Length.ToString() + " bytes, data : " + content);
         
-
-                Thread t = new Thread(()=>Send (handler, "#PWD123456"));
+                string date1 = DateTime.Now.ToString("yy/MM/dd HH:mm:ss");
+                date1.Replace(' ', ',');
+                Console.WriteLine("data1: {0} \n",date1);
+                
+                Thread t = new Thread(()=>Send (handler, "#PWD123456#ROK,"+"Ajscx4s7,"+date1));
+                
                 
                 t.Start();
                 
