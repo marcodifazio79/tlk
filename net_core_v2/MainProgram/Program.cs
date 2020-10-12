53b29a2b6281f58c8da2b895f30fe506f6d2812d
Francesco Di Fazio <fdifazio@live.com>
2020-10-12 08:45:36 +0200

    wip

--- net_core_v2/MainProgram/Program.cs
+++ net_core_v2/MainProgram/Program.cs
@@ -185,13 +185,13 @@ public class AsynchronousSocketListener {
                 //ModemsSocketList.Find( m => ((IPEndPoint)m.RemoteEndPoint).Address.ToString()   == receivedCommand.InnerXml   )
                 String targetModemIP = receivedCommand.SelectSingleNode(@"/data/targetip").InnerText;
                 String command = receivedCommand.SelectSingleNode(@"/data/command").InnerText;
-                String port = receivedCommand.SelectSingleNode(@"/data/targetport").InnerText;
+                //String port = receivedCommand.SelectSingleNode(@"/data/targetport").InnerText;
                 
                 Thread t = new Thread(()=>Send (  
                     ModemsSocketList.Find(      Soc => 
-                        ((IPEndPoint)Soc.RemoteEndPoint).Address.ToString() == targetModemIP     && 
-                        ((IPEndPoint)Soc.RemoteEndPoint).Port.ToString()    == port         )  ,
-                         command ));
+                        ((IPEndPoint)Soc.RemoteEndPoint).Address.ToString() == targetModemIP      
+                       //&& ((IPEndPoint)Soc.RemoteEndPoint).Port.ToString()    == port         )  
+                        ), command ));
                 t.Start();
 
             }
