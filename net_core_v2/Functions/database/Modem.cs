bfa96a259f12120396e78bde47b2ba8188b0925e
Francesco Di Fazio <francesco.difazio@dedem.it>
2020-11-20 08:44:53 +0100

    wip on EntityFramework

---
+++ net_core_v2/Functions/database/Modem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Functions.database
+{
+    public partial class Modem
+    {
+        public int Id { get; set; }
+        public string IpAddress { get; set; }
+        public long? Imei { get; set; }
+        public string Mid { get; set; }
+        public int? KalValue { get; set; }
+        public int? LggValue { get; set; }
+        public string Version { get; set; }
+    }
+}
---
+++ net_core_v2/Functions/database/ModemConnectionTrace.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Functions.database
+{
+    public partial class ModemConnectionTrace
+    {
+        public int Id { get; set; }
+        public string IpAddress { get; set; }
+        public string SendOrRecv { get; set; }
+        public string TransferredData { get; set; }
+    }
+}
