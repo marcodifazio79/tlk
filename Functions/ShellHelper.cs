e4d582b0523805f7a419ddaddc3bdc788ceb3f01
Francesco Fi Fazio <francesco.difazio@dedem.it>
2020-12-18 21:14:12 +0100

    wip

--- Functions/ShellHelper.cs
+++ Functions/ShellHelper.cs
@@ -19,7 +19,8 @@ namespace Functions
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = "/bin/bash",
-                    Arguments = $"-c \"{escapedArgs}\"",
+                    //Arguments = $"-c \"{escapedArgs}\"",
+                    Arguments = " -c \"dotnet /home/kdl_admin/tlk/SignalR/bin/Release/net5.0/linux-x64/publish/SignalR_.dll AskToReloadMachConnTrace 23\""
                     RedirectStandardOutput = true,
                     UseShellExecute = false,
                     CreateNoWindow = true,
