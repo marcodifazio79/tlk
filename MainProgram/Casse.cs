654325388ef70b209c8cf1dd7485b3b8132e9e0c
Francesco Di Fazio <francesco.difazio@dedem.it>
2021-04-22 09:19:18 +0200

    cominciatai i lavori per spostare la gestione delle casse dal frontend al backend

---
+++ MainProgram/Casse.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Net;  
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Xml;
+using System.Threading;
+using Functions.database;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Casse
+{
+    public class CasseFunctions
+    {   
+        public CasseFunctions()
+        {
+            
+        }
+    }
+}
