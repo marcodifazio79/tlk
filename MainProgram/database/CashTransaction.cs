0c4fc1852126d7c7edce9211e886080a8b95c607
Francesco <fdifazio@live.com>
2021-04-22 12:53:50 +0200

    implementate le funzioni per gestire le casse dai tablet. (aggiunti file mancanti)

---
+++ MainProgram/database/CashTransaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Functions.database
+{
+    public partial class CashTransaction
+    {
+        public int Id { get; set; }
+        public string Odm { get; set; }
+        public int IdMachines { get; set; }
+        public int? IdMachinesConnectionTrace { get; set; }
+        public string Status { get; set; }
+        public int? TentativiAutomaticiEseguiti { get; set; }
+        public DateTime DataCreazione { get; set; }
+        public DateTime? DataInvioRichiesta { get; set; }
+        public DateTime? DataPacchettoRicevuto { get; set; }
+        public DateTime? DataSincronizzazione { get; set; }
+        public virtual MachinesConnectionTrace IdMachinesConnectionTraceNavigation { get; set; }
+        public virtual Machines IdMachinesNavigation { get; set; }
+    }
+}
