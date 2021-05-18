4d1ca5dbfbf2e6839914e7e04984f5c2a1e9bd09
Francesco Di Fazio <francesco.difazio@dedem.it>
2021-05-18 10:54:59 +0200

    aggiunta chiusura socket per invio comandi dal fronte_end

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -216,6 +216,7 @@ public class AsynchronousSocketListener {
                 // There  might be more data, so store the data received so far.
                 state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
                 content = state.sb.ToString();
+                Console.WriteLine("Command content = "+ content);
                 if(content != "<end_of_transmission>")
                 {
                     Console.WriteLine(DateTime.Now.ToString("yy/MM/dd,HH:mm:ss") + " : Read {0} bytes from socket. Data : {1}",content.Length, content);
