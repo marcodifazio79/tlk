099c33d6f1889e17963bc427552d1e3420d76984
madifa79 <marcodifazio@gmail.com>
2021-05-27 14:21:59 +0200

    aggiunte le tabelle log*

---
+++ MainProgram/databaseeee/LogTargetRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tlk_core.databaseeee
+{
+    public partial class LogTargetRole
+    {
+        public int Id { get; set; }
+        public string IdAspNetRoles { get; set; }
+        public int IdLog { get; set; }
+
+        public virtual AspNetRoles IdAspNetRolesNavigation { get; set; }
+        public virtual Log IdLogNavigation { get; set; }
+    }
+}
