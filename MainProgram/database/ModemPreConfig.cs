167bbd1d432bb9c05d80db16796c6b9718bc1440
mdifazio <marco.difazio@dedem.it>
2021-11-25 00:30:29 +0100

    aaaaa

---
+++ MainProgram/database/ModemPreConfig.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Functions.database
+{
+    public partial class ModemPreConfig
+    {
+       
+
+        public int Id { get; set; }
+        public string IpAddress { get; set; }
+        public long? Imei { get; set; }
+        public string Mid { get; set; }
+        public DateTime? last_communication { get; set; }
+        public DateTime? time_creation { get; set; }
+
+       
+
+    }
+}
